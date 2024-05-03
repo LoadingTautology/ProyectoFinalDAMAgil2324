b2fbf6d78614b1f48cee859834ae1e166a787062
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-03 16:41:07 +0200

    Create View Registrarse

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinalDAMAgil2324.Data;
 
 namespace ProyectoFinalDAMAgil2324.Controllers
 {
     public class AccesoController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDBContext _appDbContext;
+        public AccesoController(AppDBContext appDBContext)
+        {
+            _appDbContext = appDBContext;
+        }
+
+        #region Registrarse
+
+        [HttpGet]
+        public IActionResult Registrarse()
         {
             return View();
         }
+
+        #endregion Registrarse
     }
 }
