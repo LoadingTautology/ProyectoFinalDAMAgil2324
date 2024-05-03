8fbf9d4d937d2501531b7756566aef8eb4194cb7
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-03 16:55:50 +0200

    Update UsuarioVM.cs

---
+++ ViewModels/UsuarioVM.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinalDAMAgil2324.ViewModels
+{
+    public class UsuarioVM
+    {
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string ConfirmarClave { get; set; }
+    }
+}
