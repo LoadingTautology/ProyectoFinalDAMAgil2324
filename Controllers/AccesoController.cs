547095c2e75aae696252367c1671929c113a9ce6
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-03 16:34:32 +0200

    Create AccesoController

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFinalDAMAgil2324.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
