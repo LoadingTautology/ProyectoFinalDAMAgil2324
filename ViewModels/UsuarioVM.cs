83c1722d5fcbea8a871be2c1a05ffe97ab3c076e
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-03 19:56:01 +0200

    Update UsuarioVM.cs

--- ViewModels/UsuarioVM.cs
+++ ViewModels/UsuarioVM.cs
@@ -2,9 +2,9 @@
 {
     public class UsuarioVM
     {
+        public string NombreCompleto { get; set; }
         public string Correo { get; set; }
         public string Clave { get; set; }
         public string ConfirmarClave { get; set; }
-        public string NombreCompleto { get; set; }
     }
 }
