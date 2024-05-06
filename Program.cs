46db54c40b674855f2398f09fa508e3e44df0457
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-06 18:31:53 +0200

    New branch

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Negotiate;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoFinalDAMAgil2324.Models;
 using ProyectoFinalDAMAgil2324.Services;
-using System;
+
 
 
 namespace ProyectoFinalDAMAgil2324
@@ -45,19 +44,6 @@ namespace ProyectoFinalDAMAgil2324
             });
 
 
-            ///*  BORRAR ANTES DE PONER EN PRODUCCIÓN  */
-            //builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-            //   .AddNegotiate();
-
-
-
-            //builder.Services.AddAuthorization(options =>
-            //{
-            //    // By default, all incoming requests will be authorized according to the default policy.
-            //    options.FallbackPolicy = options.DefaultPolicy;
-            //});
-            ///*  BORRAR ANTES DE PONER EN PRODUCCIÓN  */
-
             builder.Services.AddRazorPages();
 
 
@@ -86,73 +72,3 @@ namespace ProyectoFinalDAMAgil2324
         }
     }
 }
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-///* ********* Inyeccion Contexto ********* */
-//var connectionString = builder.Configuration.GetConnectionString("Default");
-//builder.Services.AddDbContext<UsuarioContext>(options => { options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)); });
-////https://medium.com/@vahidalizadeh1990/crud-operation-by-repository-pattern-using-net-6-ef-core-sql-server-mysql-mongodb-part-2-25532829b79d
-
-///* ********* Inyeccion dependencias UsuarioService ********* */
-//builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-////Con esta inyeccion de dependencias ya podemos hacer uso en cualquier controlador de la App
-
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-//{
-//    options.LoginPath = "/Login/IniciarSesion";
-//    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-//});
-
-//builder.Services.AddControllersWithViews(options =>
-//{
-//    options.Filters.Add(
-//        new ResponseCacheAttribute
-//        {
-//            NoStore = true,
-//            Location = ResponseCacheLocation.None,
-//        }
-//       );
-//});
-
-
-///*  Borrar  */
-//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-//   .AddNegotiate();
-
-
-
-//builder.Services.AddAuthorization(options =>
-//{
-//    // By default, all incoming requests will be authorized according to the default policy.
-//    options.FallbackPolicy = options.DefaultPolicy;
-//});
-///*  Borrar  */
-//builder.Services.AddRazorPages();
-
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Login}/{action=IniciarSesion}/{id?}");
-
-//app.Run();
