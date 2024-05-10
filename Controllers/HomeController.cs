73b991cea410c9c7453c1d720014a6ab82709dfe
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-10 18:12:52 +0200

    silly change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,30 +8,60 @@ using System.Security.Claims;
 
 namespace ProyectoFinalDAMAgil2324.Controllers
 {
-    [Authorize]
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	[Authorize]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
 		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+
+		public IActionResult Index()
+		{
+			ClaimsPrincipal claimsUser = HttpContext.User;
+			string nombreUsuario = "";
+
+			if (claimsUser.Identity!.IsAuthenticated)
+			{
+				nombreUsuario = claimsUser.Claims.Where(c => c.Type == ClaimTypes.Name).Select(c => c.Value).SingleOrDefault()!;
+			}
+
+			ViewData["nombreUsuario"] = nombreUsuario;
+			return View();
+		}
 
 		public IActionResult Privacy()
 		{
 			return View();
 		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+
+		public async Task<IActionResult> CerrarSesion()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Index", "MainPage");
+		}
+	}
 }
+
+/*
+ 
+             if (User.Identity!.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+ 
+            if (HttpContext.UserIdentity!.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+ */
